fe65e3f18f0d644aaaa4182397ea1b437b21982a
mahaveerrss <mahaveersain515@gmail.com>
2024-01-17 13:39:57 +0530

    fixed #3 long code to short code

--- 3-DigitalClock/chaiaurcode.js
+++ 3-DigitalClock/chaiaurcode.js
@@ -6,18 +6,10 @@ const Time = function(){
     // clock element selector ;
     const clock = document.getElementById("clock");
 
-    // Time;
-    const hour = new Date().getHours() 
-    const min = new Date().getMinutes() 
-    const sec = new Date().getSeconds() 
-     
-    // show time without prefix 0 ;
-    const otherWise =  clock.innerText = `${hour}:${min}:${sec}`;
-
-        // To add  0 as prefix for less than 10 values ;
-     (hour < 10 ) ? clock.innerText = `0${hour}:${min}:${sec}` : otherWise; 
-     (min < 10 ) ? clock.innerText = `${hour}:0${min}:${sec}` : otherWise; 
-     (sec < 10 ) ? clock.innerText = `${hour}:${min}:0${sec}` : otherWise; 
+     const time = new Date().toLocaleTimeString()
+ 
+      clock.innerText = time;
+ 
 }
 
 Time(); // To immediatly show time ;
