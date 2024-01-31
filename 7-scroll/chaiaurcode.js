58fe4133913c44af775e43a8f1b59d08f1d094e9
mahaveerrss <mahaveersain515@gmail.com>
2024-01-31 18:09:56 +0530

    made scroll indicator

--- 7-scroll/chaiaurcode.js
+++ 7-scroll/chaiaurcode.js
@@ -1,18 +1,25 @@
 // use indicator width
 
-const section = document.getElementsByName('section')[0];
+const section = document.getElementsByName("section")[0];
 
+const progressBar = document.getElementsByClassName("progress")[0];
 
+const lineIndicator = document.querySelectorAll("#lineIndicator")[0];
 
-window.addEventListener('scroll',(e) => {
-   const windowHeight = window.innerHeight ;
-  
-   e.currentTarget = 5
-    
-   
- 
- 
-   
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY) {
+    const scrollPosition = window.scrollY;
 
-}
-)
+    const docHeight = document.body.scrollHeight;
+
+    const calculated = Math.ceil(parseInt((scrollPosition / docHeight) * 100));
+    if (calculated > 95) {
+      progressBar.innerText = 100 + "%";
+      lineIndicator.style.width = "100%";
+    } else {
+      progressBar.innerText = calculated + "%";
+
+      lineIndicator.style.width = calculated + "%";
+    }
+  }
+});
