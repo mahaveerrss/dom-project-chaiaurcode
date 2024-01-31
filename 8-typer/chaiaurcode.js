7f31fda54a2fe6ecd680a921479b8a442d648d80
mahaveerrss <mahaveersain515@gmail.com>
2024-01-31 18:19:05 +0530

    added typer js

--- 8-typer/chaiaurcode.js
+++ 8-typer/chaiaurcode.js
@@ -3,13 +3,37 @@ const cursor = document.querySelector('.cursor');
 
 const words = ['Love', 'Jhakaas', 'mast', 'dhinchak', 'Weird'];
 
+ let i = 0;
+
  
 
-function type() {
+ const printer = (wordArray,time) => {
+   setInterval(()=>{
+type(wordArray)
+   },time)
+ }
+ 
+
+function type(words) {
+
+    //  prints every word per 800ms ..
+   if(i<words.length){
+   typedTextSpan.innerText = words[i]
+   
+
+    i++
+    if(i == 5){
+        i=0
+    }
+    }
+ 
   
 }
 
+printer(words,1000)
  
+
+
 function erase() {}
 
  
